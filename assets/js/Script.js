cbdb1e463b7dd99695d1419e4d907c69d24a8966
LEONARDO AUGUSTO ALBUQUERQUE <leoprostyle@gmail.com>
2022-06-27 22:14:13 -0300

    Initial commit

---
+++ assets/js/Script.js
@@ -0,0 +1,18 @@
+const phoneInputField = document.querySelector("#telefone");
+const phoneInput = window.intlTelInput(phoneInputField, {
+  utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/17.0.8/js/utils.js",
+});
+
+const cookieContainer = document.querySelector(".cookie-container");
+const cookieBotao = document.querySelector(".cookie-botao");
+
+cookieBotao.addEventListener("click", () => {
+  cookieContainer.classList.remove("active");
+  localStorage.setItem("aceitoTermos", "true");
+});
+
+setTimeout(() => {
+  if (!localStorage.getItem("aceitoTermos")) {
+    cookieContainer.classList.add("active");
+  }
+}, 2000);
