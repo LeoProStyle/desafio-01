b816d9181f49c05dbb5c65beb5044623be003874
LeoProStyle <leoprostyle@gmail.com>
2022-06-30 13:01:47 -0300

    alteracao script

--- assets/js/countries.js
+++ assets/js/countries.js
@@ -262,7 +262,7 @@ s_a[251]="Central|Copperbelt|Eastern|Luapula|Lusaka|North-Western|Northern|South
 s_a[252]="Bulawayo|Harare|ManicalandMashonaland Central|Mashonaland East|Mashonaland West|Masvingo|Matabeleland North|Matabeleland South|Midlands";
 
 function print_country(country_id){
-	// given the id of the <select> tag as function argument, it inserts <option> tags
+		
 	var option_str = document.getElementById(country_id);
 	option_str.length=0;
 	option_str.options[0] = new Option('Selecione seu País','');
@@ -273,12 +273,13 @@ function print_country(country_id){
 }
 
 function print_state(state_id, state_index){
-	var option_str = document.getElementById(state_id);
-	option_str.length=0;	// Fixed by Julian Woods
+	var option_str = document.getElementById(state_id);		
+	option_str.length=0;
 	option_str.options[0] = new Option('Selecione o Estado','');
 	option_str.selectedIndex = 0;
 	var state_arr = s_a[state_index].split("|");
 	for (var i=0; i<state_arr.length; i++) {
-		option_str.options[option_str.length] = new Option(state_arr[i],state_arr[i]);
+			option_str.options[option_str.length] = new Option(state_arr[i],state_arr[i]);
 	}
 }
+
